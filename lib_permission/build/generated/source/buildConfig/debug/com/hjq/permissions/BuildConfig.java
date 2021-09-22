60ef95f0d27eadec42775eb0f6fad8f350933202
陶海理 <15220055862@163.com>
2021-09-22 10:37:30 +0800

    Initial commit

---
+++ lib_permission/build/generated/source/buildConfig/debug/com/hjq/permissions/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.hjq.permissions;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.hjq.permissions";
+  public static final String BUILD_TYPE = "debug";
+}
